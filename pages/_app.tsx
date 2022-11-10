baffd984e8493aaaaa2d6ed6523200b8984d47ec
suman chapai <sumanchapai@gmail.com>
2022-11-10 15:02:19 -0600

    Expose functionality to change rooom from top level context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,10 +10,11 @@ import { SessionProvider } from "next-auth/react";
 interface GlobalContextType {
   network: WebrtcProvider | null;
   doc: Doc | null;
+  setRoom: (x: string) => void;
 }
 
 function useGlobalContext() {
-  return React.useState({ doc: null, network: null });
+  return React.useState({ doc: null, network: null, setRoom: (string) => {} });
 }
 export const GlobalContext = React.createContext<GlobalContextType | null>(
   null
@@ -25,11 +26,18 @@ export default function MyApp({
 }: AppProps): JSX.Element {
   const [globalContextValue, setGlobalContextValue] = useGlobalContext();
   const [isBrowser, setIsBrowser] = React.useState(false);
+  const [room, setRoom] = React.useState("default-room-to-not-use");
   React.useEffect(() => {
     setIsBrowser(true);
     const ydoc = new Doc();
-    const networkProvider = new WebrtcProvider("syn-global-room", ydoc);
-    setGlobalContextValue({ network: networkProvider, doc: ydoc });
+    const networkProvider = new WebrtcProvider(room, ydoc);
+    setGlobalContextValue({
+      network: networkProvider,
+      doc: ydoc,
+      setRoom: (x: string) => {
+        setRoom(x);
+      },
+    });
     new IndexeddbPersistence("syn-index-db", ydoc);
   }, []);
   return (
