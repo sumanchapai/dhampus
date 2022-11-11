e57088d4764645d500ed4d7dc894bbbc4969b97f
suman chapai <sumanchapai@gmail.com>
2022-11-11 08:46:10 -0600

    Fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export function PromptVirutalRoom({ isOpen, setIsOpen }) {
               type="submit"
               className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
             >
-              Star Check-In
+              Start Check-In
             </button>
             <button
               type="submit"
