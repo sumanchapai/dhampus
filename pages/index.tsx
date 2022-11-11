fb90c7786c3e1e4d49092244a66fa73bb4c76528
suman chapai <sumanchapai@gmail.com>
2022-11-10 18:54:55 -0600

    Extract Dialog into a separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { Dialog, Transition } from "@headlessui/react";
 import { useRouter } from "next/router";
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
+import MyDialog from "../components/Dialog";
 import { InputField, InputLabelGroup, Label } from "../components/Form";
 
 export default function Home() {
@@ -41,70 +41,32 @@ export function PromptVirutalRoom({ isOpen, setIsOpen }) {
   }
 
   return (
-    <>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Code
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <form action="" onSubmit={handleSubmit}>
-                      <InputLabelGroup>
-                        {Label("code", "Please enter your Check-In code.")}
-                        {InputField("code", "text", code, (e) => {
-                          setCode(e.target.value);
-                        })}
-                      </InputLabelGroup>
-                      <div className="flex gap-x-4">
-                        <button
-                          type="submit"
-                          className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                        >
-                          Star Check-In
-                        </button>
-                        <button
-                          type="submit"
-                          className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-                          onClick={closeModal}
-                        >
-                          Go Back
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+    <MyDialog isOpen={isOpen} closeModal={closeModal} titleString="Code">
+      <div className="mt-2">
+        <form action="" onSubmit={handleSubmit}>
+          <InputLabelGroup>
+            {Label("code", "Please enter your Check-In code.")}
+            {InputField("code", "text", code, (e) => {
+              setCode(e.target.value);
+            })}
+          </InputLabelGroup>
+          <div className="flex gap-x-4">
+            <button
+              type="submit"
+              className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            >
+              Star Check-In
+            </button>
+            <button
+              type="submit"
+              className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+              onClick={closeModal}
+            >
+              Go Back
+            </button>
           </div>
-        </Dialog>
-      </Transition>
-    </>
+        </form>
+      </div>
+    </MyDialog>
   );
 }
